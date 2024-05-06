a20c5c5cae35a13a5a8214c1526e970208b1777b
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-05-06 10:59:25 +0100

    ft: add the felx map data type

---
+++ server/connecto.server/Models/FlexMap.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+namespace connecto.server.Models;
+
+public class FlexMap : Dictionary<string, object>
+{
+    private const string IdKey = "id";
+
+    public bool HasId() => ContainsKey(IdKey);
+
+    public Guid? Id()
+    {
+        if (!HasId())
+            return null;
+
+        var rawId = this[IdKey].ToString();
+        return Guid.Parse(rawId!);
+    }
+
+    public string Serialize()
+    {
+        Remove(IdKey);
+        return JsonSerializer.Serialize(this);
+    }
+}
