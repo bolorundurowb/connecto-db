4026d55e1654628138236ce7894e2970cd0b0ee2
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-05-11 21:59:32 +0100

    ft: add data stream hub implementation

--- server/Config.cs
+++ server/Config.cs
@@ -3,4 +3,8 @@
 public static class Config
 {
     public const string DbName = "connecto.db";
+
+    public const string EntityUpdated = nameof(EntityUpdated);
+
+    public const string EntityCreated = nameof(EntityCreated);
 }
--- server/Hubs/DataStreamHub.cs
+++ server/Hubs/DataStreamHub.cs
@@ -1,8 +1,40 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using connecto.server.Models;
+using connecto.server.Services;
+using Microsoft.AspNetCore.SignalR;
 
 namespace connecto.server.Hubs;
 
 public class DataStreamHub : Hub
 {
-    
+    private readonly Dictionary<string, DataService> _services = new();
+
+    public async Task UpsertDataRecord(string tableName, FlexMap req)
+    {
+        var service = GetServiceForTable(tableName);
+
+        if (req.HasId())
+        {
+            await service.Update(req);
+            await Clients.All.SendAsync(Config.EntityUpdated, tableName, req);
+        }
+        else
+        {
+            var id = await service.Create(req);
+            var entity = await service.GetById(id);
+            await Clients.All.SendAsync(Config.EntityCreated, tableName, entity);
+        }
+    }
+
+    private DataService GetServiceForTable(string tableName)
+    {
+        var hasKey = _services.ContainsKey(tableName);
+
+        if (hasKey) 
+            return _services[tableName];
+
+        var service = new DataService(Config.DbName, tableName);
+        _services.Add(tableName, service);
+
+        return service;
+    }
 }
