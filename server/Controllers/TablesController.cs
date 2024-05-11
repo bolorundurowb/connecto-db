5cfdec589247705b8a6abedb0d87411715d162db
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-05-11 22:11:08 +0100

    ft: add table controllers

---
+++ server/Controllers/TablesController.cs
@@ -0,0 +1,18 @@
+﻿using connecto.server.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace connecto.server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TablesController : ControllerBase
+{
+    private readonly TableService _tableService = new(Config.DbName);
+
+    [HttpGet("")]
+    public async Task<IActionResult> GetAll()
+    {
+        var tables = await _tableService.GetAll();
+        return Ok(tables);
+    }
+}
--- server/Program.cs
+++ server/Program.cs
@@ -26,4 +26,6 @@ app.UseAuthorization();
 
 app.MapHub<DataStreamHub>("/data-stream");
 
+app.MapControllers();
+
 app.Run();
