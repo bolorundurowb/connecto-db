66553d9d1f4a87d610105acc5f9e1ce5c3ed8c3f
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2024-05-24 20:40:06 +0100

    ft: add in req models

---
+++ server/Models/Req/LoginReq.cs
@@ -0,0 +1,8 @@
+namespace connecto.server.Models.Req;
+
+public class LoginReq
+{
+    public string Username { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+}
---
+++ server/Models/Req/RegisterReq.cs
@@ -0,0 +1,12 @@
+namespace connecto.server.Models.Req;
+
+public class RegisterReq
+{
+    public string Username { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+}
